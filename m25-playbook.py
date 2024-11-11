7ec336c2c0932173db87c50fcbaf5a787e6f0bca
kasteelion <119770784+kasteelion@users.noreply.github.com>
2024-11-10 21:54:55 -0500

    Finding teams

--- m25-playbook.py
+++ m25-playbook.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 # Send a GET request to the webpage
 # url = "https://www.madden-school.com/playbooks/"
 url = "https://www.madden-school.com/playbooks/packers/defense/4-3/even-6-1/"
@@ -11,9 +12,12 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 #print(f"Printing soup {soup}")
 
+# Create an list for teams
+teams = []
 
 
-target_string = "defense/4-3/even-6-1"
+
+target_string = "defense" or "offense"
 
 for a in soup.find_all('a'):
     href = a.get('href')
@@ -22,5 +26,5 @@ for a in soup.find_all('a'):
     
     if href and href.startswith('/playbooks/') and (amountOfSlashes == 6) and target_string in href:
        url_parts = href.split('/')
-       thingwewant = '/'.join(url_parts[4:])
+       thingwewant = ','.join(url_parts[4:])
        print(f"URL is: {thingwewant}")
